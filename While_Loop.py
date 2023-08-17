10a78bd39b8825c2645e67fe55cdbcb14a5e8bd8
suleymagination <suleymagination@gmail.com>
2023-08-17 09:36:56 +0700

    For Loop

---
+++ While_Loop.py
@@ -0,0 +1,7 @@
+Jumlah_Buku = 10
+print("""Ibu berkata: "Baca semua bukumu!" """)
+
+for buku in range(Jumlah_Buku):
+    print(f"Buku ke {buku+1} sudah dibaca")
+
+print(f"Jumlah buku yang saya baca = {buku+1}")
