2898a4e664c33dd73dab68ee8c9db203b02e92ab
suleymagination <suleymagination@gmail.com>
2023-08-24 14:11:03 +0700

    Dictionary (follow JSON format)

--- Dictionary.py
+++ Dictionary.py
@@ -2,10 +2,25 @@ users = {
     "id": 1,
     "name": "Leanne Graham",
     "username": "Bret",
-    "email": "Sincere@april.biz"
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
 }
 print(users)
 print(users["id"])
 print(users["name"])
 print(users["username"])
 print(users["email"])
+print()
+print(users["address"])
+print(users["address"]["street"])
+print(users["address"]["city"])
+print(users["address"]["geo"]["lat"])
