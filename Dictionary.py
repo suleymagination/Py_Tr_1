8759ddbbb29e9989b1cecbf3228425df581b17bd
suleymagination <suleymagination@gmail.com>
2023-08-24 16:30:42 +0700

    Dictionary (follow JSON format)

--- Dictionary.py
+++ Dictionary.py
@@ -14,7 +14,8 @@ users = {
         }
     }
 }
-print(users)
+print(f"users = {users}")
+print(f"Data type users = {type(users)}")
 print(users["id"])
 print(users["name"])
 print(users["username"])
@@ -24,3 +25,15 @@ print(users["address"])
 print(users["address"]["street"])
 print(users["address"]["city"])
 print(users["address"]["geo"]["lat"])
+
+print("\nPrint to JSON format:")
+import json
+users_json = json.dumps(users)
+print(f"users = {users_json}")
+print(f"Data type users = {type(users_json)}")
+
+print("\nPrint to JSON file:")
+# Open a file for writing
+with open("users_json.json", "w") as file:
+    # Write the dictionary to the file
+    json.dump(users_json, file)
